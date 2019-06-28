0cb3ace879d1b4630db60dc4c6e3938f7ad83855
Yaser Nicaragua <yanv1991@gmail.com>
2019-06-28 14:46:28 -0600

    add css and value to display

--- src/components/Display/Display.jsx
+++ src/components/Display/Display.jsx
@@ -1,7 +1,13 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const Display = ({ displayValue }) => <div className="display-container" />;
+import './Display.css';
+
+const Display = ({ displayValue }) => <div className="display-container">
+    <p className="display-value">
+      {displayValue}
+    </p>
+</div>;
 
 Display.propTypes = { displayValue: PropTypes.string.isRequired };
 
